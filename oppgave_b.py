cd576652e32e32c90af51d5822927572363c4640
Herman <78619090+HermanErKu@users.noreply.github.com>
2023-04-26 19:43:09 +0200

    Kommenterte kode
    Enklere å lese

--- oppgave_b.py
+++ oppgave_b.py
@@ -1,16 +1,22 @@
+# Importerer alle nødvendige libraries
 import turtle, keyboard
 
 
+# Setter alle turtle innstillingene riktig
 turtle.speed(speed=10000)
 turtle.showturtle()
 turtle.title('Tentamen Oppgave 4b')
 
-n = 3
-height = 10
-squareSize = 35
+
+# Innstillinger for hvordan tegningen kommer til å se ut
+n = 3                   # Hvilket nummer figur skal den tegne
+height = 10             # Hvilken høyde skal tegningen starte på
+squareSize = 35         # Hvor stor skal firkantene i tegningen være
 
 
+# Klasse med figurer som programmet kan tegne
 class objects:
+    # Funksjon for å tegne en firkant
     def square(size:int, xPos:int, yPos:int):
         turtle.penup()
         turtle.goto(xPos, yPos)
@@ -19,6 +25,8 @@ class objects:
             turtle.forward(size)
             turtle.right(90)
 
+            
+    # Funksjon for å tegne en trekant
     def triangle(size:int, xPos:int, yPos:int):
         turtle.penup()
         turtle.goto(xPos, yPos)
@@ -27,6 +35,8 @@ class objects:
             turtle.forward(size)
             turtle.right(120)
 
+            
+    # Funksjon for å tegne en strek
     def line(xPosStart:int, yPosStart:int, xPosEnd:int, yPosEnd:int):
         turtle.penup()
         turtle.goto(xPosStart, yPosStart)
@@ -34,6 +44,7 @@ class objects:
         turtle.goto(xPosEnd, yPosEnd)
 
 
+# Finner posisjon og tegner de 3 første firkantene, 1 på hver side av figuren og 1 på toppen av figuren
 startPosLastSquare = (n * squareSize) + squareSize
 startPosFirstSquare = (-startPosLastSquare) + n*squareSize
 startPosTopSquare = (n * squareSize) / 2
@@ -43,14 +54,15 @@ objects.square(squareSize, startPosTopSquare, squareSize+10)
 
 nn = (n+1)*squareSize
 
-for i in range(0, nn, squareSize):
-    for x in range(0, nn, squareSize):
+
+for i in range(0, nn, squareSize):                  # Looper gjennom hvor mange firkanter det skal være nedover i det midterste kvadratet  (n+1)*(n+1)  og tegner en firkant
+    for x in range(0, nn, squareSize):              # Looper gjennom hvor mange firkanter det skal være bortover i det midterste kvadratet  (n+1)*(n+1)  og tegner en firkant
         objects.square(squareSize, x, height)
     height -= squareSize 
 
 
+# Lukker vindu med tegning hvis du trykker på 'q'
 turtle.hideturtle()
-
 isPressed = False
 while isPressed == False:
     if keyboard.is_pressed('q'):
